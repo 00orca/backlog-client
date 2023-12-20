f598ad0950a72a5aadc5eed27954a38658ce072f
Willhem <libanwillhem@gmail.com>
2023-12-20 20:04:43 +0100

    get all the game data as props, add line ellipsis for game title

--- src/Components/Game/GameCard.jsx
+++ src/Components/Game/GameCard.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./_style.module.scss";
+import LinesEllipsis from "react-lines-ellipsis";
 
 function GameCard(props) {
-  const { name, image_id } = props;
+  const { game } = props;
+  const { name, cover, platforms } = game;
+  const image = cover?.image_id;
+
   return (
     <div className={styles["game-card-container"]}>
       <div className={styles["game-card-container__image-wrapper"]}>
-        {image_id && (
+        {image && (
           <img
-            src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${image_id}.jpg`}
+            src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${image}.jpg`}
             alt={name}
           />
         )}
       </div>
       <div className={styles["game-card-container__body"]}>
-        <div className={styles["game-card-container__body__name"]}>{name}</div>
-        <div className={styles["game-card-container__body__plateforms"]}>
-          {/* <span>PS4</span>
-          <span>PC</span>
-          <span>Switch</span> */}
+        <div className={styles["game-card-container__body__name"]}>
+          <LinesEllipsis
+            text={name}
+            maxLine={2}
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+          />
+        </div>
+        <div className={styles["game-card-container__body__consoles"]}>
+          {platforms?.slice(0, 3).map((platform) => (
+            <div
+              className={styles["game-card-container__body__consoles__item"]}
+              key={platform.id}
+            >
+              {platform.abbreviation}
+            </div>
+          ))}
+          {platforms?.length > 3 && (
+            <div
+              className={styles["game-card-container__body__consoles__item"]}
+            >
+              +{platforms.length - 3}
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -27,13 +51,11 @@ function GameCard(props) {
 }
 
 GameCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  image_id: PropTypes.string.isRequired,
+  game: PropTypes.object.isRequired,
 };
 
 GameCard.defaultProps = {
-  name: "",
-  image_id: "",
+  game: {},
 };
 
 export default GameCard;
